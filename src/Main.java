59674070120308a0af5d7b596ea1b29a103cd993
NuriDiu <117601581+NuriDiu@users.noreply.github.com>
2023-03-26 22:46:56 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        
+
+        Worker.OnTaskDoneListener listener = System.out::println;
+        Worker.OnTaskErrorListener listener2 = System.out::println;
+
+        Worker worker = new Worker(listener);
+        Worker worker2 = new Worker(listener2);
+        worker.start();
+        worker2.start2();
 }
